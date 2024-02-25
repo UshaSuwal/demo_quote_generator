c44af1044afc6f1ec4a4c84c1a21afb6fcbc0bb9
UshaSuwal <usaasuwa@gmail.com>
2024-02-25 18:33:13 +0545

    changed select tag for dropdown

--- script.js
+++ script.js
@@ -17,189 +17,187 @@ const quotes = {
     ]
   };
   
-let currentCategory = 'inspiration';
-let currentIndex = 0;
-let currentFontSize = 20;
-let darkModeEnabled = false;
-
-const quoteText = document.getElementById('quoteText');
-const categorySelect = document.getElementById('categorySelect');
-const prevBtn = document.getElementById('prevBtn');
-const nextBtn = document.getElementById('nextBtn');
-const randomBtn = document.getElementById('randomBtn');
-const darkModeToggle = document.getElementById('darkModeToggle');
-const increaseFontBtn = document.getElementById('increaseFontBtn');
-const decreaseFontBtn = document.getElementById('decreaseFontBtn');
-const copyBtn = document.getElementById("copyBtn");
-const checkBtn = document.getElementById("checkBtn");
-
-
-categorySelect.addEventListener('change', (event) => {
-    currentCategory = event.target.value;
+  let currentCategory = 'inspiration';
+  let currentIndex = 0;
+  let currentFontSize = 20;
+  let darkModeEnabled = false;
+  
+  const quoteText = document.getElementById('quoteText');
+  const categoryContainer = document.getElementById('customDropdown');
+  const dropdownHeader = document.querySelector('.dropdown-header');
+  const dropdownList = document.getElementById('categoryList');
+  
+  dropdownHeader.addEventListener('click', () => {
+    dropdownList.style.display = (dropdownList.style.display === 'block') ? 'none' : 'block';
+  });
+  
+  function changeCategory(category) {
+    currentCategory = category;
     currentIndex = 0;
     renderQuote();
-});
-
-prevBtn.addEventListener('click', () => {
-    if (currentIndex > 0) {
-        currentIndex--;
-        renderQuote();
+    dropdownList.style.display = 'none';
+  }
+  
+  dropdownList.addEventListener('click', (event) => {
+    if (event.target.tagName === 'LI') {
+      const selectedCategory = event.target.textContent.toLowerCase();
+      changeCategory(selectedCategory);
     }
-});
-
-nextBtn.addEventListener('click', () => {
-    if (currentIndex < quotes[currentCategory].length - 1) {
-        currentIndex++;
-        renderQuote();
-    }
-});
-
-randomBtn.addEventListener('click', () => {
-    currentIndex = Math.floor(Math.random() * quotes[currentCategory].length);
-    renderQuote();
-});
+  });
+  
+  const prevBtn = document.getElementById('prevBtn');
+  const nextBtn = document.getElementById('nextBtn');
+  const randomBtn = document.getElementById('randomBtn');
+  const darkModeToggle = document.getElementById('darkModeToggle');
+  const increaseFontBtn = document.getElementById('increaseFontBtn');
+  const decreaseFontBtn = document.getElementById('decreaseFontBtn');
+  const copyBtn = document.getElementById("copyBtn");
+  const checkBtn = document.getElementById("checkBtn");
+  
+  categoryContainer.addEventListener('change', (event) => {
+      currentCategory = event.target.value;
+      currentIndex = 0;
+      renderQuote();
+  });
+  
+  prevBtn.addEventListener('click', () => {
+      if (currentIndex > 0) {
+          currentIndex--;
+          renderQuote();
+      }
+  });
+  
+  nextBtn.addEventListener('click', () => {
+      if (currentIndex < quotes[currentCategory].length - 1) {
+          currentIndex++;
+          renderQuote();
+      }
+  });
+  
+  randomBtn.addEventListener('click', () => {
+      currentIndex = Math.floor(Math.random() * quotes[currentCategory].length);
+      renderQuote();
+  });
 
 // darkModeToggle.addEventListener('click', () => {
 //     darkModeEnabled = !darkModeEnabled;
 //     toggleDarkMode();
 // });
-
-darkModeToggle.addEventListener('change', () => {
-    document.body.classList.toggle('dark-mode');
-    toggleGlowAnimation();
-    updateImageSource();
-});
-
-function toggleGlowAnimation() {
-    const quoteContainer = document.querySelector('.quote-container');
-    
-    if (document.body.classList.contains('dark-mode')) {
-        quoteContainer.classList.add('white-glow');
-        quoteContainer.classList.remove('black-glow');
-    } else {
-        quoteContainer.classList.add('black-glow');
-        quoteContainer.classList.remove('white-glow');
-    }
-}
-
-function updateImageSource() {
-    const logoImage = document.getElementById('logoImage');
-    const isDarkMode = document.body.classList.contains('dark-mode');
+  
+  darkModeToggle.addEventListener('change', () => {
+      document.body.classList.toggle('dark-mode');
+      toggleGlowAnimation();
+      updateImageSource();
+  });
+  
+  function toggleGlowAnimation() {
+      const quoteContainer = document.querySelector('.quote-container');
+      
+      if (document.body.classList.contains('dark-mode')) {
+          quoteContainer.classList.add('white-glow');
+          quoteContainer.classList.remove('black-glow');
+      } else {
+          quoteContainer.classList.add('black-glow');
+          quoteContainer.classList.remove('white-glow');
+      }
+  }
+  
+  function updateImageSource() {
+      const logoImage = document.getElementById('logoImage');
+      const isDarkMode = document.body.classList.contains('dark-mode');
+  
+// Set the image source based on dark mode
+      logoImage.src = isDarkMode ? 'images/whiteBook.png' : 'images/book.png';
+  
+      const logo = document.getElementById('logo');
 
     // Set the image source based on dark mode
-    logoImage.src = isDarkMode ? 'images/whiteBook.png' : 'images/book.png';
-
-    const logo = document.getElementById('logo');
-
-    // Set the image source based on dark mode
-    logo.src = isDarkMode ? 'images/whiteLogo.png' : 'images/logo.png';
-}
-
-increaseFontBtn.addEventListener('click', () => {
-    if (currentFontSize < 28) {
-    currentFontSize += 1;
-    quoteText.style.fontSize = `${currentFontSize}px`;
-    authorNameContent.style.fontSize = `${currentFontSize}px`;
-    }
-});
-
-decreaseFontBtn.addEventListener('click', () => {
-    if (currentFontSize > 16) {
-        currentFontSize -= 1;
-        quoteText.style.fontSize = `${currentFontSize}px`;
-        authorNameContent.style.fontSize = `${currentFontSize}px`;
-    }
-});
-
-function renderQuote() {
-    const quote = quotes[currentCategory][currentIndex];
-    const [quoteText, authorName] = quote.split(' - '); // Splitting the quote into text and author
-
-    // Update the quote text and author name elements
-    document.getElementById('quoteText').textContent = `"${quoteText}"`; // Displaying the quote text with double quotes
-    document.getElementById('authorNameContent').textContent = `- ${authorName}`; // Displaying the author name with a dash prefix
-}
-renderQuote();
-
-// function toggleDarkMode() {
-//     if (darkModeEnabled) {
-//         document.body.classList.add('dark-mode');
-//         darkModeToggle.classList.add('fa-sun'); // Change moon to sun
-//         darkModeToggle.classList.remove('fa-moon'); // Remove moon class
-//     } else {
-//         document.body.classList.remove('dark-mode');
-//         darkModeToggle.classList.remove('fa-sun'); // Remove sun class
-//         darkModeToggle.classList.add('fa-moon'); // Change sun to moon
-//     }
-// }
-
-
-// Function to read the quote aloud
-function readQuote() {
-    const quote = document.getElementById('quoteText').textContent;
-    const speech = new SpeechSynthesisUtterance(quote);
-    speech.lang = 'en-US';
-    window.speechSynthesis.speak(speech);
-}
-
+      logo.src = isDarkMode ? 'images/whiteLogo.png' : 'images/logo.png';
+  }
+  
+  increaseFontBtn.addEventListener('click', () => {
+      if (currentFontSize < 28) {
+          currentFontSize += 1;
+          quoteText.style.fontSize = `${currentFontSize}px`;
+authorNameContent.style.fontSize = `${currentFontSize}px`;
+      }
+  });
+  
+  decreaseFontBtn.addEventListener('click', () => {
+      if (currentFontSize > 16) {
+          currentFontSize -= 1;
+          quoteText.style.fontSize = `${currentFontSize}px`;
+authorNameContent.style.fontSize = `${currentFontSize}px`;
+      }
+  });
+  
+  function renderQuote() {
+      const quote = quotes[currentCategory][currentIndex];
+      const [quoteTextContent, authorName] = quote.split(' -');
+  
+      quoteText.textContent = `"${quoteTextContent.trim()}"`;
+      authorNameContent.textContent = `- ${authorName.trim()}`;
+  }
+  renderQuote();
+  
+  copyBtn.addEventListener("click", function () {
+      checkBtn.style.display = "block";
+      copyBtn.style.display= "none";
+  
+      setTimeout(function () {
+          checkBtn.style.display = "none";
+          copyBtn.style.display= "block";
+      }, 2000);
+  });
+  
+  function readQuote() {
+      const quote = document.getElementById('quoteText').textContent;
+      const speech = new SpeechSynthesisUtterance(quote);
+      speech.lang = 'en-US';
+      window.speechSynthesis.speak(speech);
+  }
+  
 // Function to copy the quote text to clipboard
-function copyQuote() {
-    const quote = document.getElementById('quoteText').textContent;
-    navigator.clipboard.writeText(quote)
-        .catch(err => console.error('Failed to copy text: ', err));
-}
-
-
-    
-
-copyBtn.addEventListener("click", function () {
-    // Simulate copying action
-    checkBtn.style.display = "block";
-    copyBtn.style.display= "none"
-     // Display the check icon
-
-    // Hide the check icon after 2 seconds
-    setTimeout(function () {
-        checkBtn.style.display = "none";
-        copyBtn.style.display= "block"
-    }, 2000);
-});
-
-
-// Event listeners for read aloud and copy buttons
-document.getElementById('readBtn').addEventListener('click', readQuote);
-document.getElementById('copyBtn').addEventListener('click', copyQuote);
-
+  function copyQuote() {
+      const quote = document.getElementById('quoteText').textContent;
+      navigator.clipboard.writeText(quote)
+          .catch(err => console.error('Failed to copy text: ', err));
+  }
+  
+  document.getElementById('readBtn').addEventListener('click', readQuote);
+  document.getElementById('copyBtn').addEventListener('click', copyQuote);
+  
 
 
 
 
 // stars
-document.addEventListener("DOMContentLoaded", function() {
-    const numberOfStars = 20;
-    const container = document.body;
-
-    for (let i = 0; i < numberOfStars; i++) {
-        createStar();
-    }
-
-    function createStar() {
-        const star = document.createElement("div");
-        star.className = "star";
-
-        const size = Math.random() * 3 + 1; // Random size between 1 and 4
-        star.style.width = `${size}px`;
-        star.style.height = `${size}px`;
-
-        const posX = Math.random() * window.innerWidth;
-        const posY = Math.random() * window.innerHeight;
-        star.style.left = `${posX}px`;
-        star.style.top = `${posY}px`;
-
-        const duration = Math.random() * 1 + 0.5; // Random duration between 0.5 and 1.5 seconds
-        const delay = Math.random() * 1; // Random delay between 0 and 1 second
-        star.style.animation = `glow ${duration}s ${delay}s infinite alternate`;
-
-        container.appendChild(star);
-    }
-});
+  document.addEventListener("DOMContentLoaded", function() {
+      const numberOfStars = 20;
+      const container = document.body;
+  
+      for (let i = 0; i < numberOfStars; i++) {
+          createStar();
+      }
+  
+      function createStar() {
+          const star = document.createElement("div");
+          star.className = "star";
+  
+          const size = Math.random() * 3 + 1; // Random size between 1 and 4
+          star.style.width = `${size}px`;
+          star.style.height = `${size}px`;
+  
+          const posX = Math.random() * window.innerWidth;
+          const posY = Math.random() * window.innerHeight;
+          star.style.left = `${posX}px`;
+          star.style.top = `${posY}px`;
+  
+          const duration = Math.random() * 1 + 0.5; // Random duration between 0.5 and 1.5 seconds
+          const delay = Math.random() * 1; // Random delay between 0 and 1 second
+          star.style.animation = `glow ${duration}s ${delay}s infinite alternate`;
+  
+          container.appendChild(star);
+      }
+  });
+  
